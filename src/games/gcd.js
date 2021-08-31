fd4daa2889e200d225b6f0d7d02ad3f08e05b104
Oleh Yuzva <olegyuzva22@gmail.com>
2021-08-31 12:47:40 +0300

    fix bugs

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,18 +2,18 @@ import theGame from '../secondary/theGame.js';
 import randomNumber from '../secondary/randomNumber.js';
 
 const commonDivisor = (num1, num2) => {
-	if (!num2) {
-		return num1;
-	}
-	return commonDivisor(num2, num1 % num2);
+  if (!num2) {
+    return num1;
+  }
+  return commonDivisor(num2, num1 % num2);
 };
 
 const generateQuestionAnswer = () => {
-	const questionNumber1 = randomNumber();
-	const questionNumber2 = randomNumber();
-	const questionAndAnswer = `Question: ${questionNumber1} ${questionNumber2}\nYour answer: `;
-	const correctAnswer = String(commonDivisor(questionNumber1, questionNumber2));
-	return [questionAndAnswer, correctAnswer];
+  const questionNumber1 = randomNumber();
+  const questionNumber2 = randomNumber();
+  const questionAndAnswer = `Question: ${questionNumber1} ${questionNumber2}\nYour answer: `;
+  const correctAnswer = String(commonDivisor(questionNumber1, questionNumber2));
+  return [questionAndAnswer, correctAnswer];
 };
 
 const rules = 'Find the greatest common divisor of given numbers.';
